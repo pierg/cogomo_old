6cb69f04af199b048c2b21408a2d40a78da476ac
pierg <nmall843r>
2020-05-13 22:13:29 +0200

    dockerfilemods

--- src/controller/strix.py
+++ src/controller/strix.py
@@ -6,7 +6,7 @@ from checks.tools import And, Not, Implies
 def get_controller(assumptions: str, guarantees: str, ins: str, outs: str) -> str:
     try:
         params = ' -f "' + Implies(assumptions, guarantees) + '" --ins="' + ins + '" --outs="' + outs + '"'
-        print(params)
+        print("\n\n" + params + "\n\n")
         output = subprocess.check_output(['strix', params], encoding='UTF-8', stderr=subprocess.DEVNULL)
         print("\n\nOUTPUT\n\n" + output)
         return output
