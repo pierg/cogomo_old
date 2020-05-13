71da41c7ede6e336f6045659180ff9b5c8309e24
pierg <nmall843r>
2020-05-13 22:08:54 +0200

    dockerfilemods

--- tests/controller/controller.py
+++ tests/controller/controller.py
@@ -8,7 +8,58 @@ file_path = os.path.dirname(os.path.abspath(__file__)) + "/clustering"
 
 if __name__ == "__main__":
 
-    a, g, i, o = parse_controller(file_path + "/controller-input.txt")
-    controller = get_controller(a, g, i, o)
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
 
-    print(controller)
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_0.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
+
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_1.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
+
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_2.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
+
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_3.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
+
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_4.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
+
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_5.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
+
+    try:
+        a, g, i, o = parse_controller(file_path + "/controller-input_6.txt")
+        controller = get_controller(a, g, i, o)
+        print(controller)
+    except Exception as e:
+        "Exception"
