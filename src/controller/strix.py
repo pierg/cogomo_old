e62d61ae02514405b96e8dcf9f89d4d797576009
pierg <nmall843r>
2020-05-13 22:12:24 +0200

    dockerfilemods

--- src/controller/strix.py
+++ src/controller/strix.py
@@ -8,7 +8,8 @@ def get_controller(assumptions: str, guarantees: str, ins: str, outs: str) -> st
         params = ' -f "' + Implies(assumptions, guarantees) + '" --ins="' + ins + '" --outs="' + outs + '"'
         print(params)
         output = subprocess.check_output(['strix', params], encoding='UTF-8', stderr=subprocess.DEVNULL)
+        print("\n\nOUTPUT\n\n" + output)
+        return output
     except Exception as e:
         raise e
-    print("\n\nOUTPUT\n\n" + output)
-    return output
+
