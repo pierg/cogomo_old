b6b47fed0790ba73dd60f012c1fa49baf50cc873
pierg <nmall843r>
2020-05-14 09:40:24 +0200

    small fix

--- src/controller/strix.py
+++ src/controller/strix.py
@@ -1,15 +1,16 @@
+import os
 import subprocess
 from typing import Tuple, List, Union
 from checks.tools import And, Not, Implies
 
+strix_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', 'bin', 'ubuntu_19.10', 'strix'))
 
 def get_controller(assumptions: str, guarantees: str, ins: str, outs: str) -> str:
     try:
         params = ' -f "' + Implies(assumptions, guarantees) + '" --ins="' + ins + '" --outs="' + outs + '"'
         print("\n\n" + params + "\n\n")
-        output = subprocess.check_output(['strix', params], encoding='UTF-8', stderr=subprocess.DEVNULL)
+        output = subprocess.check_output([strix_path, params], encoding='UTF-8', stderr=subprocess.DEVNULL)
         print("\n\nOUTPUT\n\n" + output)
         return output
     except Exception as e:
         raise e
-
