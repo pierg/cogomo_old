01a0a34b53ec1626937b24a546fef1912ae2b719
pierg <nmall843r>
2020-05-14 19:48:39 +0200

    small fix

--- run.sh
+++ run.sh
@@ -1,1 +1,31 @@
-bash
+# Generate Files
+if [ $# -eq 0 ]
+  then
+    echo "No parameters provided. Launching bash"
+    bash
+else
+    while test $# -gt 0; do
+      case "$1" in
+        -h|--help)
+          echo "$package [options] application [arguments]"
+          echo " "
+          echo "options:"
+          echo "-h, --help                show brief help"
+          echo "-c,                       launch clustering"
+          echo "-m,                       launch mapping"
+          exit 0
+          ;;
+        -c)
+          echo "Launching clustering..."
+          python3 ./clustering_run.py
+          ;;
+        -m)
+          echo "Launching mapping..."
+          python3 ./mapping_run.py
+          ;;
+        *)
+          break
+          ;;
+      esac
+    done
+fi
