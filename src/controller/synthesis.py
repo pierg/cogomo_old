eb644445c54fa2836079b8319e2a3a828bd590d2
pierg <nmall843r>
2020-05-14 10:05:06 +0200

    small fix

--- src/controller/synthesis.py
+++ src/controller/synthesis.py
@@ -13,7 +13,7 @@ output_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'
 def get_controller(assumptions: str, guarantees: str, ins: str, outs: str) -> str:
     try:
         params = ' -f "' + Implies(assumptions, guarantees) + '" --ins="' + ins + '" --outs="' + outs + '"'
-        print("\n\n" + params + "\n\n")
+        print("\n\nCOMMAND:\n\n" + strix_path + params + "\n\n")
         output = subprocess.check_output([strix_path, params], encoding='UTF-8', stderr=subprocess.DEVNULL)
         print("\n\nOUTPUT\n\n" + output)
         return output
