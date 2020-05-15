29888a1fb387d7a355ae80996d493624d316d65f
pierg <nmall843r>
2020-05-15 14:05:50 +0200

    small fix

--- run_clustering_docker.sh
+++ run_clustering_docker.sh
@@ -1,5 +1,5 @@
 
 # To modfiy the mission in input modify the input_clustering.py in ./output
 docker run  -it \
-            -v "$(pwd)/output":/home/cogomo/output \
+            -V "$(pwd)/output":/home/cogomo/output \
             pmallozzi/cogomo:latest
