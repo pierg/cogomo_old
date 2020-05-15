008b74af87662c81503c41b4e9aa3c802f38e468
pierg <nmall843r>
2020-05-15 14:41:28 +0200

    small fix

--- run_scripts/run_clustering_docker.sh
+++ run_scripts/run_clustering_docker.sh
@@ -6,7 +6,7 @@ docker stop cogomo_clustering || true && docker rm cogomo_clustering || true
 docker create --name cogomo_clustering -v "$(pwd)/$1":/home/cogomo/output pmallozzi/cogomo:latest -c
 
 # Copy the input file
-docker cp "$(pwd)/$1/clustering_mission_default.py" cogomo_clustering:/home/cogomo/
+docker cp "$(pwd)/$1/clustering_mission_custom.py" cogomo_clustering:/home/cogomo/
 
 # Start the container in clustering mode
 docker start cogomo_clustering
